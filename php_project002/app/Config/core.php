93c66a7f2978ec89212addad04a382fa21f73b69
nishimura.takeshi <nishimura.takeshi@zeevaa.com>
2014-05-27 18:29:25 +0900

    初期処理

--- php_project002/app/Config/core.php
+++ php_project002/app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'xfs2guVDYhG93b0qFgaC9miyJfIoUubWwvniR2G0');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '76896759309657645453542449683');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
